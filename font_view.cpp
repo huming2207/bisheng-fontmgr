2d9ed5741379d8640ea6b50694416dfedaf24be2
Jackson Ming Hu <jackson@smartguide.com.au>
2022-05-27 16:19:49 +1000

    font_view: integrate with font cacher

--- font_view.cpp
+++ font_view.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <esp_heap_caps.h>
 #include "font_view.hpp"
+#include "font_cacher.hpp"
 
 bool font_view::get_glyph_dsc_handler(const lv_font_t *font, lv_font_glyph_dsc_t *dsc_out, uint32_t unicode_letter, uint32_t unicode_letter_next)
 {
@@ -46,16 +47,35 @@ const uint8_t *font_view::get_glyph_bitmap_handler(const lv_font_t *font, uint32
 
     auto *ctx = (font_view *)font->user_data;
     float scale = ctx->scale;
+    auto &cache = font_cacher::instance();
 
-    int width = 0, height = 0;
-    if (stbtt_GetCodepointBitmapPtr(&ctx->stb_font, scale, scale, (int)unicode_letter, ctx->font_buf, &width, &height, nullptr, nullptr)) {
-        return ctx->font_buf;
+    if (ctx->font_buf == nullptr) {
+        glyph_item glyph{};
+        auto ret = cache.get_cache(ctx->renderer_id, unicode_letter, &glyph);
+        if (ret == ESP_OK) {
+            return glyph.bitmap;
+        } else if (ret == ESP_ERR_NOT_FOUND) {
+            int width = 0, height = 0;
+            auto *buf = stbtt_GetCodepointBitmap(&ctx->stb_font, scale, scale, (int)unicode_letter, &width, &height, nullptr, nullptr);
+            if (buf != nullptr) {
+                cache.add_cache(ctx->renderer_id, unicode_letter, buf, ctx->height_px * ctx->height_px);
+            }
+
+            return buf;
+        } else {
+            return nullptr;
+        }
     } else {
-        return nullptr;
+        int width = 0, height = 0;
+        if (stbtt_GetCodepointBitmapPtr(&ctx->stb_font, scale, scale, (int)unicode_letter, ctx->font_buf, &width, &height, nullptr, nullptr)) {
+            return ctx->font_buf;
+        } else {
+            return nullptr;
+        }
     }
 }
 
-esp_err_t font_view::init(const uint8_t *buf, size_t len, uint8_t _height_px)
+esp_err_t font_view::init(const uint8_t *buf, size_t len, uint8_t _height_px, bool disable_cache)
 {
     height_px = _height_px;
     lv_font.line_height = height_px;
@@ -65,20 +85,28 @@ esp_err_t font_view::init(const uint8_t *buf, size_t len, uint8_t _height_px)
     lv_font.base_line = 0;
     lv_font.subpx = LV_FONT_SUBPX_NONE;
 
+    auto &cache = font_cacher::instance();
+    renderer_id = cache.get_new_renderer_id();
+
     if (stbtt_InitFont(&stb_font, buf, 0) < 1) {
         ESP_LOGE(TAG, "Load font failed");
         return ESP_FAIL;
     }
 
-    ESP_LOGD(TAG, "Allocating font buffer size %u bytes", _height_px * _height_px);
+    if (disable_cache) {
+        ESP_LOGD(TAG, "Allocating font buffer size %u bytes", _height_px * _height_px);
 
-    font_buf = (uint8_t *)heap_caps_malloc(_height_px * _height_px, MALLOC_CAP_SPIRAM);
-    if (font_buf == nullptr) {
-        ESP_LOGE(TAG, "Failed to allocate font buffer");
-        return ESP_ERR_NO_MEM;
+        font_buf = (uint8_t *)heap_caps_malloc(_height_px * _height_px, MALLOC_CAP_SPIRAM);
+        if (font_buf == nullptr) {
+            ESP_LOGE(TAG, "Failed to allocate font buffer");
+            return ESP_ERR_NO_MEM;
+        }
+
+        memset(font_buf, 0, _height_px * _height_px);
+    } else {
+        font_buf = nullptr;
     }
 
-    memset(font_buf, 0, _height_px * _height_px);
     scale = stbtt_ScaleForPixelHeight(&stb_font, height_px);
     return ESP_OK;
 }
--- includes/font_view.hpp
+++ includes/font_view.hpp
@@ -17,12 +17,13 @@ public:
     static const uint8_t *get_glyph_bitmap_handler(const lv_font_t *font, uint32_t unicode_letter);
 
 public:
-    esp_err_t init(const uint8_t *buf, size_t len, uint8_t _height_px);
+    esp_err_t init(const uint8_t *buf, size_t len, uint8_t _height_px, bool disable_cache);
     esp_err_t decorate_font_style(lv_style_t *style);
 
 private:
     uint8_t height_px = 0;
     uint8_t *font_buf = nullptr;
+    uint32_t renderer_id = 0;
     float scale = 0;
     lv_font_t lv_font = {};
     stbtt_fontinfo stb_font = {};
