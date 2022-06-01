ed5f8979c4602c0a7b9c55c32773a6a3d4c37593
Jackson Ming Hu <jackson@smartguide.com.au>
2022-06-01 17:11:16 +1000

    stbtt_mem: check allocation is successful or not

--- stbtt_mempool.c
+++ stbtt_mempool.c
@@ -1,15 +1,23 @@
 #include <stdbool.h>
 #include <esp_heap_caps.h>
+#include <esp_log.h>
 
 #include "stbtt_mempool.h"
 
 #define STBTT_MEM_INIT_SIZE 131072
+
+#ifdef TAG
+#undef TAG
+#endif
+
+#define TAG "stbtt_heap"
 
 static void *stbtt_main_heap = NULL;
 
 esp_err_t stbtt_mem_init()
 {
     if (stbtt_main_heap) {
+        ESP_LOGI(TAG, "Heap already initialised");
         return ESP_OK;
     }
 
@@ -21,10 +29,16 @@ esp_err_t stbtt_mem_init()
 #endif
 
     if (stbtt_main_heap == NULL) {
+        ESP_LOGI(TAG, "Failed to alloc heap");
         return ESP_ERR_NO_MEM;
     }
 
     stbtt_mem_tlsf = stbtt_tlsf_create_with_pool(stbtt_main_heap, STBTT_MEM_INIT_SIZE);
+    if (stbtt_mem_tlsf == NULL) {
+        ESP_LOGE(TAG, "Failed to create heap mem pool");
+        return ESP_ERR_NO_MEM;
+    }
+
     return ESP_OK;
 }
 
